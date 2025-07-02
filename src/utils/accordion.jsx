ccaf89af49da535361f197f0c28141107d09cbc0
Toluddev <toluwalaseoludipe7@gmail.com>
2025-07-02 18:24:59 +0100

    first commit

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Competitive interest rates you can count on",
+    detail:
+      "Our interest rates are designed to give you the best value — competitive, transparent, and tailored to fit your financial goals.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Maintain pricing consistency you can trust",
+    detail:
+      "We help you secure long-term value by protecting your investment from unpredictable market changes.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Exceptional homes at the right price",
+    detail:
+      "We offer a wide range of properties that meet your needs and budget, ensuring you find the perfect home without compromise.",
+  },
+];
+export default data;
